58679496c3cc89b2b5eb96baedb0e22adfe06f65
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-12-01 15:58:45 +0100

    fix: package name in error

--- src/NativeSkottieModule.ts
+++ src/NativeSkottieModule.ts
@@ -6,7 +6,7 @@ import type {
 import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'react-native-skia-skottie' doesn't seem to be linked. Make sure: \n\n` +
+  `The package 'react-native-skottie' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
