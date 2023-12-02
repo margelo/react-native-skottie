58f477a6cc93bafbadd84fb69f2c8a63aa052fb7
Hanno J. Gödecke <die.drei99@yahoo.de>
2023-12-02 12:02:03 +0100

    chore(dev): clean example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,28 +1,19 @@
 import * as React from 'react';
 
-import { StyleSheet, SafeAreaView, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import { SkiaSkottieView } from 'react-native-skottie';
 import LottieAnimation from './Hands.json';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.flex1}>
-      <View
-        style={{
-          height: 500,
-          width: 300,
-          backgroundColor: 'lightgrey',
-        }}
-      >
-        <SkiaSkottieView
-          resizeMode="contain"
-          style={styles.flex1}
-          src={LottieAnimation}
-          autoPlay={true}
-          loop={false}
-          onAnimationFinish={() => console.log('Animation finished')}
-        />
-      </View>
+      <SkiaSkottieView
+        resizeMode="contain"
+        style={styles.flex1}
+        src={LottieAnimation}
+        autoPlay={true}
+        loop={true}
+      />
     </SafeAreaView>
   );
 }
@@ -30,7 +21,5 @@ export default function App() {
 const styles = StyleSheet.create({
   flex1: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
